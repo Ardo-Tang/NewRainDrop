3a2e42774e7b8bf0fc18ebde74c3525e7221b367
Ardo-Tang <z7193028@gmail.com>
2020-04-07 17:05:32 +0800

    black pixels.

--- Randrop.py
+++ Randrop.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
 import sys
 import os
 import shutil
 import math
+
 
 class Raindrop(object):
 
@@ -18,13 +19,14 @@ class Raindrop(object):
             end = frames
 
         video = self.Reader(YUVpath, frames, rows, cols)
-        
-        print("Rain drop removal...", end="")
-        Y = np.array([i[0] for i in video]) #(frame, rows, cols)
-        newY = np.array(Y)
-        referMaps = [Y[i] for i in range(len(Y)) if(i<referFrames)]
 
-        meanMap, leftMeanMap = self.__ReferFramesStatistics(referMaps, rows, cols)
+        print("Rain drop removal...", end="")
+        Y = np.array([i[0] for i in video])  # (frame, rows, cols)
+        newY = np.array(Y)
+        referMaps = [Y[i] for i in range(len(Y)) if(i < referFrames)]
+
+        meanMap, leftMeanMap = self.__ReferFramesStatistics(
+            referMaps, rows, cols)
         # print(leftMeanMap)
         # sys.exit()
         for frame in range(referFrames, end):
@@ -33,7 +35,8 @@ class Raindrop(object):
                 for y in range(cols):
                     if(Y[frame, x, y] > meanMap[x, y]):
                         try:
-                            newY[frame,x,y] = int(leftMeanMap[x,y,0])
+                            newY[frame, x, y] = int(leftMeanMap[x, y, 0])
+                            # print("leftMeanMap",int(leftMeanMap[x,y,0]))
                         except:
                             # print(leftMeanMap[x,y])
                             pass
@@ -44,18 +47,19 @@ class Raindrop(object):
             # 更新(待改進)
             oldestMap = referMaps.pop(0)
             referMaps.append(Y[frame])
-            meanMap, leftMeanMap = self.__Refresh(referMaps, meanMap, leftMeanMap, oldestMap, Y[frame], rows, cols)
-        
+            meanMap, leftMeanMap = self.__Refresh(
+                referMaps, meanMap, leftMeanMap, oldestMap, Y[frame], rows, cols)
+
         # 寫回
         for frame in range(end):
             video[frame] = [newY[frame], video[frame][1], video[frame][2]]
-        
+
         self.video = video
         print("\rRain drop removal...OK.             ")
-    
+
     def Exporter(self, outputPath, frames, rows, cols):
         print("Export...")
-        fp = open(outputPath,'wb')
+        fp = open(outputPath, 'wb')
 
         uv_rows = rows//2
         uv_cols = cols//2
@@ -73,7 +77,7 @@ class Raindrop(object):
 
     def Reader(self, YUVpath, frames, rows, cols):
         print("Reading...", end="")
-        fp = open(YUVpath,'rb')
+        fp = open(YUVpath, 'rb')
 
         uv_rows = rows//2
         uv_cols = cols//2
@@ -85,10 +89,10 @@ class Raindrop(object):
             start = 0
             end = frames
 
-        allFrames = []        
+        allFrames = []
         for frame in range(end):
             print("\rReading...", frame, end="")
-            if(frame<start):
+            if(frame < start):
                 continue
             else:
                 Y = np.zeros((rows, cols))
@@ -96,72 +100,82 @@ class Raindrop(object):
                 V = np.zeros((uv_rows, uv_cols))
                 for m in range(rows):
                     for n in range(cols):
-                        Y[m,n] = ord(fp.read(1))
+                        Y[m, n] = ord(fp.read(1))
                 for m in range(uv_rows):
                     for n in range(uv_cols):
-                        V[m,n] = ord(fp.read(1))
+                        V[m, n] = ord(fp.read(1))
                 for m in range(uv_rows):
                     for n in range(uv_cols):
-                        U[m,n] = ord(fp.read(1))
+                        U[m, n] = ord(fp.read(1))
 
                 allFrames.append([Y, U, V])
 
         fp.close()
         print("\rReading...OK.                    ")
-        return allFrames #(frame, [Y,U,V], rows, cols)
+        return allFrames  # (frame, [Y,U,V], rows, cols)
 
     def __Refresh(self, referMaps, meanMap, leftMeanMap, oldestMap, newestMap, rows, cols):
+        # def pixelWiseProcessing(row, col):
+
         leftMeanMap_out = leftMeanMap
         meanMap_out = meanMap
+        
         for row in range(rows):
             for col in range(cols):
-                popInAndOut = (newestMap[row,col]-oldestMap[row,col]) / len(referMaps)
-                if(oldestMap[row,col]>meanMap[row,col]):
-                    if(newestMap[row,col]>meanMap[row,col]):
+                popInAndOut = (newestMap[row, col] -
+                               oldestMap[row, col]) / len(referMaps)
+
+                if(oldestMap[row, col] > meanMap[row, col]):
+                    if(newestMap[row, col] > meanMap[row, col]):
                         pass
                     else:
-                        if(math.isnan(leftMeanMap[row,col,0])):
-                            leftMeanMap_out[row,col,0] = (newestMap[row,col]) / 1
-                            leftMeanMap_out[row,col,1] = 1
+                        if(leftMeanMap[row, col, 1] == 0):
+                            leftMeanMap_out[row, col, 1] = leftMeanMap[row, col, 1] + 1
+                            leftMeanMap_out[row, col, 0] = (newestMap[row, col]) / leftMeanMap_out[row, col, 1]
                         else:
-                            leftMeanMap_out[row,col,0] = (leftMeanMap[row,col,0]*leftMeanMap[row,col,1] + newestMap[row,col]) / (leftMeanMap[row,col,1]+1)
-                            leftMeanMap_out[row,col,1] = leftMeanMap[row,col,1] + 1
+                            leftMeanMap_out[row, col, 1] = leftMeanMap[row, col, 1] + 1
+                            leftMeanMap_out[row, col, 0] = (leftMeanMap[row, col, 0]*leftMeanMap[row, col, 1] + newestMap[row, col]) / leftMeanMap_out[row, col, 1]
+
                 else:
-                    if(newestMap[row,col]>meanMap[row,col]):
-                        if(math.isnan(leftMeanMap[row,col,0])):
-                            pass
+                    if(newestMap[row, col] > meanMap[row, col]):
+                        if(leftMeanMap[row, col, 1] < 1):
+                            leftMeanMap_out[row, col, 1] = leftMeanMap_out[row, col, 1]
+                            leftMeanMap_out[row, col, 0] = meanMap[row, col]
                         else:
-                            leftMeanMap_out[row,col,0] = (leftMeanMap[row,col,0]*leftMeanMap[row,col,1] - oldestMap[row,col]) / (leftMeanMap[row,col,1]-1)
-                            leftMeanMap_out[row,col,1] = leftMeanMap[row,col,1] - 1
+                            leftMeanMap_out[row, col, 1] = leftMeanMap[row, col, 1] - 1
+                            leftMeanMap_out[row, col, 0] = (leftMeanMap[row, col, 0]*leftMeanMap[row, col, 1] - oldestMap[row, col]) / leftMeanMap_out[row, col, 1]
                     else:
-                        if(math.isnan(leftMeanMap[row,col,0])):
-                            pass
+                        if(leftMeanMap[row, col, 1] == 0):
+                            leftMeanMap_out[row, col, 1] = 0
+                            leftMeanMap_out[row, col, 0] = meanMap[row, col]
                         else:
-                            leftMeanMap_out[row,col,0] = (leftMeanMap[row,col,0]*leftMeanMap[row,col,1] - oldestMap[row,col] + newestMap[row,col]) / leftMeanMap[row,col,1]
-                
-                meanMap_out[row,col] = meanMap[row,col] + popInAndOut 
-                
-                if(math.isnan(leftMeanMap_out[row,col,0]) and leftMeanMap_out[row,col,1]>1):
-                    print(leftMeanMap_out[row,col])
-                    print(newestMap[row,col])
-                    print(oldestMap[row,col])
-                    sys.exit()
+                            leftMeanMap_out[row, col, 1] = leftMeanMap[row, col, 1] + 1 - 1
+                            leftMeanMap_out[row, col, 0] = (leftMeanMap[row, col, 0]*leftMeanMap[row, col, 1] - oldestMap[row, col] + newestMap[row, col]) / leftMeanMap_out[row, col, 1]
+
+                meanMap_out[row, col] = meanMap[row, col] + popInAndOut
+
+                # if(math.isnan(leftMeanMap_out[row, col, 0]) or leftMeanMap_out[row, col, 1] == 0):
+                #     print(leftMeanMap_out[row, col])
+                #     print(newestMap[row, col])
+                #     print(oldestMap[row, col])
+                #     sys.exit()
         return meanMap_out, leftMeanMap_out
 
     def __ReferFramesStatistics(self, referMaps, rows, cols):
         meanMap = np.zeros((rows, cols))
-        leftMeanMap = np.zeros((rows,cols,2))
+        leftMeanMap = np.zeros((rows, cols, 2))
 
         for row in range(rows):
             for col in range(cols):
                 temp = np.array([frame[row, col] for frame in referMaps])
                 meanMap[row, col] = temp.mean()
-                leftTemp = np.array([frame[row, col] for frame in referMaps if(frame[row, col]<meanMap[row, col])])
-                leftMeanMap[row,col,0] = leftTemp.mean()
-                leftMeanMap[row,col,1] = len(leftTemp)
+                leftTemp = np.array([frame[row, col] for frame in referMaps if(
+                    frame[row, col] < meanMap[row, col])])
+                leftMeanMap[row, col, 0] = leftTemp.mean()
+                leftMeanMap[row, col, 1] = len(leftTemp)
 
         return meanMap, leftMeanMap
-    
+
     def __FolderChecker(self, path):
         if not(os.path.isdir(path)):
             os.mkdir(path)
@@ -169,16 +183,16 @@ class Raindrop(object):
             shutil.rmtree(path)
             os.mkdir(path)
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     inputPath = "demo.yuv"
     frames = 753
     rows = 288
     cols = 352
-    
+
     raindrop = Raindrop()
     raindrop.RainDropRemoval(inputPath, frames, rows, cols, referFrames=50)
     raindrop.Exporter("./out/out.yuv", frames, rows, cols)
 
     # raindrop.video = raindrop.Reader(inputPath, 100, rows, cols)
     # raindrop.Exporter("./out/out.yuv", 100, rows,cols)
-
